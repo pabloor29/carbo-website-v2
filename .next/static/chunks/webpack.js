7d1204aca1c4c20306be437909585b200514329b
pabloor29 <pab.ortg@gmail.com>
2025-03-30 19:03:47 +0200

    feat:

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f434fb7b3b36462"; }
+/******/ 		__webpack_require__.h = function() { return "3898c7ccc53c713b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d2ef16f5de2c6319"; }
+/******/ 	__webpack_require__.h = function() { return "b4a48300c56b2b24"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
